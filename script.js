219d66408d8164a176f579d10635424776de15cf
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-16 22:01:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function promiseFactory() {
+	const time = Math.floor(Math.random() * 5) + min;
+	return new Promise((resolve, reject) => {
+		resolve(`I got resolved after ${time} seconds`);
+	}, time * 1000);
+}
+
+for(let i = 0; i < n; i++) {
+	promises.push(promiseFactory());
+}
+
+Promise
+	.any(promises)
+	.then(value => document.getElementById('output').textContent = value);
